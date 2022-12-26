442cb73533f63dfb608b9add7d95a64f91619669
Alex <113362960+AlexPolarBear@users.noreply.github.com>
2022-12-26 15:46:28 +0300

    Update Bytecode_reader.cpp

--- Bytecode_reader.cpp
+++ Bytecode_reader.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#iclude "../PLTools/Lama/blob/1.10/byterun/byterun.c"
 
 using namespace std;
 
 int main() {
 	unsigned int x;
 	unsigned char b;
-	ifstream myFile("example.txt", ios::out | ios::binary);
+	ifstream myFile(bytefile, ios::out | ios::binary);
 	myFile.seekg(0, ios::beg);
 	myFile.read(reinterpret_cast<char*>(&b), sizeof(b));
 	x = (unsigned int)b;
